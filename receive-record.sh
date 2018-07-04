6ee075649a3b706b6a711a0e85dfbfbe71daa65a
Garrett Weaver <wagarrettweaver@gmail.com>
2018-07-04 11:09:53 -0700

    Added audio transmission and recording.

--- receive-record.sh
+++ receive-record.sh
@@ -2,8 +2,12 @@
 
 OUTPUT_FILE_NAME=`date +%Y-%m-%d.%H:%M:%S`.mp4
 
-gst-launch-1.0 -v udpsrc multicast-group=224.1.1.1 auto-multicast=true port=5001 ! application/x-rtp ! rtph264depay ! tee name=t \
-	t. ! queue ! vaapih264dec ! autovideosink sync=false \
-	t. ! queue ! h264parse ! mpegtsmux name=mux ! queue max-size-buffers=0 max-size-bytes=0 max-size-time=0 ! filesink location=$OUTPUT_FILE_NAME
+gst-launch-1.0 -v udpsrc multicast-group=224.1.1.1 auto-multicast=true port=5001 ! application/x-rtp ! rtph264depay ! tee name=v \
+	v. ! queue ! vaapih264dec ! autovideosink sync=false \
+	udpsrc multicast-group=224.1.1.1 auto-multicast=true port=5002 ! application/x-rtp ! rtpmp4adepay ! tee name=a \
+	a. ! queue ! avdec_aac ! audioconvert ! autoaudiosink \
+	a. ! aacparse ! mux. \
+	v. ! queue ! h264parse ! mux. \
+	mpegtsmux name=mux ! queue max-size-buffers=0 max-size-bytes=0 max-size-time=0 ! filesink location=$OUTPUT_FILE_NAME
 
 
--- transmit.sh
+++ transmit.sh
@@ -7,5 +7,7 @@ while ! ping -c 1 -W 1 192.168.1.1; do
 done
 
 # Once the network is up, then start the script
-gst-launch-1.0 -v v4l2src device=/dev/video0 ! queue ! video/x-h264,width=1920,height=1080,framerate=30/1 ! h264parse ! rtph264pay ! udpsink host=224.1.1.1 port=5001 auto-multicast=true
+gst-launch-1.0 -v \
+	v4l2src device=/dev/video0 ! queue ! video/x-h264,width=1920,height=1080,framerate=30/1 ! h264parse ! rtph264pay ! udpsink host=224.1.1.1 port=5001 auto-multicast=true \
+	alsasrc device=hw:1 ! audioconvert ! queue ! voaacenc ! rtpmp4apay ! udpsink host=224.1.1.1 port=5002 auto-multicast=true 
 
