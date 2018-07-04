d54c1144452d9fbd2f55df0a70993496be9acb9c
Garrett Weaver <wagarrettweaver@gmail.com>
2018-07-04 11:18:05 -0700

    Added caps for audio.

--- receive-record.sh
+++ receive-record.sh
@@ -4,8 +4,8 @@ OUTPUT_FILE_NAME=`date +%Y-%m-%d.%H:%M:%S`.mp4
 
 gst-launch-1.0 -v udpsrc multicast-group=224.1.1.1 auto-multicast=true port=5001 ! application/x-rtp ! rtph264depay ! tee name=v \
 	v. ! queue ! vaapih264dec ! autovideosink sync=false \
-	udpsrc multicast-group=224.1.1.1 auto-multicast=true port=5002 ! application/x-rtp ! rtpmp4adepay ! tee name=a \
-	a. ! queue ! avdec_aac ! audioconvert ! autoaudiosink \
+	udpsrc multicast-group=224.1.1.1 auto-multicast=true port=5002 ! application/x-rtp, media=audio, clock-rate=32000, encoding-name=MP4A-LATM, payload=96 ! rtpmp4adepay ! tee name=a \
+	a. ! queue ! avdec_aac ! audioconvert ! autoaudiosink sync=false \
 	a. ! aacparse ! mux. \
 	v. ! queue ! h264parse ! mux. \
 	mpegtsmux name=mux ! queue max-size-buffers=0 max-size-bytes=0 max-size-time=0 ! filesink location=$OUTPUT_FILE_NAME
