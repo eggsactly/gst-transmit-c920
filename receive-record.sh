2319fb126fe733e41e73e19f990634263bcac0b8
Garrett Weaver <wagarrettweaver@gmail.com>
2018-07-04 11:25:13 -0700

    Rearranged lines in receive-record.

--- receive-record.sh
+++ receive-record.sh
@@ -2,12 +2,12 @@
 
 OUTPUT_FILE_NAME=`date +%Y-%m-%d.%H:%M:%S`.mp4
 
-gst-launch-1.0 -v udpsrc multicast-group=224.1.1.1 auto-multicast=true port=5001 ! application/x-rtp ! rtph264depay ! tee name=v \
-	v. ! queue ! vaapih264dec ! autovideosink sync=false \
+gst-launch-1.0 -v \
+	udpsrc multicast-group=224.1.1.1 auto-multicast=true port=5001 ! application/x-rtp ! rtph264depay ! tee name=v \
 	udpsrc multicast-group=224.1.1.1 auto-multicast=true port=5002 ! application/x-rtp, media=audio, clock-rate=32000, encoding-name=MP4A-LATM, payload=96 ! rtpmp4adepay ! tee name=a \
+	v. ! queue ! vaapih264dec ! autovideosink sync=false \
 	a. ! queue ! avdec_aac ! audioconvert ! autoaudiosink sync=false \
-	a. ! aacparse ! mux. \
 	v. ! queue ! h264parse ! mux. \
+	a. ! aacparse ! mux. \
 	mpegtsmux name=mux ! queue max-size-buffers=0 max-size-bytes=0 max-size-time=0 ! filesink location=$OUTPUT_FILE_NAME
-
 
