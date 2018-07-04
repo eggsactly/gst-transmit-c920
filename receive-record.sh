42c61bbc1cd0c3ae9f10400345b33783851afd43
Garrett Weaver <wagarrettweaver@gmail.com>
2018-07-04 15:56:41 -0700

    Changed pipeline to send/receive raw audio.

--- receive-record.sh
+++ receive-record.sh
@@ -4,10 +4,10 @@ OUTPUT_FILE_NAME=`date +%Y-%m-%d.%H:%M:%S`.mp4
 
 gst-launch-1.0 -v \
 	udpsrc multicast-group=224.1.1.1 auto-multicast=true port=5001 ! application/x-rtp ! rtph264depay ! tee name=v \
-	udpsrc multicast-group=224.1.1.1 auto-multicast=true port=5002 ! application/x-rtp, media=audio, clock-rate=32000, encoding-name=MP4A-LATM, payload=96 ! rtpmp4adepay ! tee name=a \
+	udpsrc multicast-group=224.1.1.1 port=5002 caps="application/x-rtp,channels=(int)2,format=(string)S16LE,media=(string)audio,payload=(int)96,clock-rate=(int)32000,encoding-name=(string)L24" ! rtpL24depay ! audioconvert ! tee name=a \
 	v. ! queue ! vaapih264dec ! autovideosink sync=false \
-	a. ! queue ! avdec_aac ! audioconvert ! autoaudiosink sync=false \
+	a. ! autoaudiosink sync=false \
 	v. ! queue ! h264parse ! mux. \
-	a. ! aacparse ! mux. \
+	a. ! voaacenc ! aacparse ! mux. \
 	mpegtsmux name=mux ! queue max-size-buffers=0 max-size-bytes=0 max-size-time=0 ! filesink location=$OUTPUT_FILE_NAME
 
