3c6062e0e86268e5940bf709ffed3e230e449fef
Garrett Weaver <wagarrettweaver@gmail.com>
2018-07-05 23:25:17 -0700

    Used mulaw to attempt to compress audio stream going out.

--- receive-record.sh
+++ receive-record.sh
@@ -5,9 +5,10 @@ AUDIO_FILE_NAME=`date +%Y-%m-%d.%H:%M:%S`.mp4
 
 gst-launch-1.0 -v \
 	udpsrc multicast-group=224.1.1.1 auto-multicast=true port=5001 ! application/x-rtp ! rtph264depay ! tee name=v \
-	udpsrc multicast-group=224.1.1.1 port=5002 caps="application/x-rtp,channels=(int)2,format=(string)S16LE,media=(string)audio,payload=(int)96,clock-rate=(int)32000,encoding-name=(string)L24" ! rtpL24depay ! audioconvert ! tee name=a \
+	udpsrc multicast-group=224.1.1.1 port=5002 caps="application/x-rtp,channels=(int)2,media=(string)audio,payload=(int)96,clock-rate=(int)32000" ! rtppcmudepay ! queue ! mulawdec ! tee name=a \
 	v. ! queue ! vaapih264dec ! autovideosink sync=false \
-	a. ! autoaudiosink sync=false \
+	a. ! queue ! audioconvert ! autoaudiosink sync=false \
 	v. ! queue ! h264parse ! mux. \
+	a. ! queue ! audioconvert ! voaacenc ! aacparse ! mux. \
 	mpegtsmux name=mux ! queue max-size-buffers=0 max-size-bytes=0 max-size-time=0 ! filesink location=$VIDEO_FILE_NAME
 
