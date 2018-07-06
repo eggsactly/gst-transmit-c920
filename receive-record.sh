9e401b8bb1dc4da74e12d2eb7451139df8314a40
Garrett Weaver <wagarrettweaver@gmail.com>
2018-07-05 23:30:52 -0700

    Added rtpjitterbuffer after udp sources.

--- receive-record.sh
+++ receive-record.sh
@@ -4,8 +4,8 @@ VIDEO_FILE_NAME=`date +%Y-%m-%d.%H:%M:%S`.mp4
 AUDIO_FILE_NAME=`date +%Y-%m-%d.%H:%M:%S`.mp4
 
 gst-launch-1.0 -v \
-	udpsrc multicast-group=224.1.1.1 auto-multicast=true port=5001 ! application/x-rtp ! rtph264depay ! tee name=v \
-	udpsrc multicast-group=224.1.1.1 port=5002 caps="application/x-rtp,channels=(int)2,media=(string)audio,payload=(int)96,clock-rate=(int)32000" ! rtppcmudepay ! queue ! mulawdec ! tee name=a \
+	udpsrc multicast-group=224.1.1.1 auto-multicast=true port=5001 ! application/x-rtp ! rtpjitterbuffer ! rtph264depay ! tee name=v \
+	udpsrc multicast-group=224.1.1.1 port=5002 caps="application/x-rtp,channels=(int)2,media=(string)audio,payload=(int)96,clock-rate=(int)32000" ! rtpjitterbuffer ! rtppcmudepay ! queue ! mulawdec ! tee name=a \
 	v. ! queue ! vaapih264dec ! autovideosink sync=false \
 	a. ! queue ! audioconvert ! autoaudiosink sync=false \
 	v. ! queue ! h264parse ! mux. \
