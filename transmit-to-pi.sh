09461c275d9018cbe3f55391687c5b9c79eb9017
Garrett Weaver <wagarrettweaver@gmail.com>
2018-07-08 21:09:42 -0700

    Got transmission to work by using x264enc instead of vaapih264enc.

--- transmit-to-pi.sh
+++ transmit-to-pi.sh
@@ -2,5 +2,6 @@
 
 # Get video from USB2.0 VGA UVC WebCam built into Asus laptop
 
-gst-launch-1.0 -v v4l2src device=/dev/video0 ! video/x-raw,width=640,height=480,framerate=20/1,format=YUY2 ! videoconvert ! queue ! x264enc ! rtph264pay ! udpsink host=224.1.1.1 port=5003 auto-multicast=true 
+gst-launch-1.0 -v \
+	v4l2src device=/dev/video0 ! video/x-raw,width=640,height=480,framerate=20/1,format=YV12 ! videoconvert ! queue ! x264enc ! h264parse ! rtph264pay pt=96 ! udpsink host=224.1.1.1 port=5004 auto-multicast=true
 
