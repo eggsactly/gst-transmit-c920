08f27890f83a7c3d19e2b4e36acb217095908d52
Garrett Weaver <wagarrettweaver@gmail.com>
2018-07-10 22:20:59 -0700

    Made pi ip address match.

--- receive-pi.sh
+++ receive-pi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gst-launch-1.0 -v udpsrc multicast-group=224.1.1.1 auto-multicast=true port=5003 ! application/x-rtp ! rtpjitterbuffer ! rtph264depay ! h264parse ! queue ! omxh264dec ! fbdevsink 
+gst-launch-1.0 -v udpsrc multicast-group=224.1.1.1 auto-multicast=true port=5004 ! application/x-rtp ! rtpjitterbuffer ! rtph264depay ! h264parse ! queue ! omxh264dec ! fbdevsink 
 
