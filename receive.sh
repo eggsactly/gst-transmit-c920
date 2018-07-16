694aca8f4683487f4e2cab583f9f4b88fb967a01
Garrett Weaver <wagarrettweaver@gmail.com>
2018-07-15 17:07:58 -0700

    Commented audio conversion of receive script because it was causing audio not to progress.

--- receive.sh
+++ receive.sh
@@ -6,7 +6,7 @@ gst-launch-1.0 -v udpsrc multicast-group=224.1.1.1 auto-multicast=true port=5001
 	v. ! queue ! vaapih264dec ! autovideosink sync=false \
 	udpsrc multicast-group=224.1.1.1 auto-multicast=true port=5002 ! application/x-rtp, media=audio, clock-rate=8000, encoding-name=PCMU ! rtpjitterbuffer ! rtppcmudepay ! queue ! mulawdec ! tee name=a \
 	a. ! audioconvert ! autoaudiosink \
-	a. ! audioconvert ! queue ! voaacenc ! aacparse ! mux. \
+#	a. ! audioconvert ! queue ! voaacenc ! aacparse ! queue ! mux. \
 	v. ! queue ! h264parse ! mux. \
-	mpegtsmux name=mux ! queue max-size-buffers=0 max-size-bytes=0 max-size-time=0 ! filesink location=$OUTPUT_FILE_NAME
+	mpegtsmux name=mux ! queue max-size-buffers=0 max-size-bytes=0 max-size-time=0 ! filesink location=$OUTPUT_FILE_NAME sync=false
 
