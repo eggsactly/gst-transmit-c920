fd45ddadccb4f13b4380785d8467b152dc71a60c
Garrett Weaver <wagarrettweaver@gmail.com>
2018-07-15 17:11:22 -0700

    Made transmit-to-pi script use hardware accelerated video compression.

--- transmit-to-pi.sh
+++ transmit-to-pi.sh
@@ -3,5 +3,5 @@
 # Get video from USB2.0 VGA UVC WebCam built into Asus laptop
 
 gst-launch-1.0 -v \
-	v4l2src device=/dev/video0 ! video/x-raw,width=640,height=480,framerate=20/1,format=YV12 ! videoconvert ! queue ! x264enc ! h264parse ! rtph264pay pt=96 ! udpsink host=224.1.1.1 port=5004 auto-multicast=true
+	v4l2src device=/dev/video0 ! video/x-raw,width=640,height=480,framerate=20/1,format=YV12 ! videoconvert ! queue ! vaapih264enc ! h264parse ! rtph264pay pt=96 ! udpsink host=224.1.1.1 port=5004 auto-multicast=true
 
